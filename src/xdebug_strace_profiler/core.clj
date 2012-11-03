07b710af1376c0667c3af2f0f3195a7dffd03af5
Anton Logvinenko <logvinenko@farpost.com>
2012-11-03 20:11:18 +1100

    i'm not in love with regexps, i'm in love with sexps

--- src/xdebug_strace_profiler/core.clj
+++ src/xdebug_strace_profiler/core.clj
@@ -5,11 +5,34 @@
 
 
 (defn get-traces [file-path]
-  (-> file-path file reader line-seq))
+  (->> file-path file reader line-seq (drop 1)))
 
-(defn parse-traces [file]  
-  file)
 
+
+
+(defn get-trace-time [trace]
+  (nth trace 0))
+
+(defn get-trace-level [trace]
+  42)
+
+(defn get-trace-call [trace]
+  (nth trace 3))
+
+(defn split-trace [trace]
+  (-> trace (.replaceAll " +" " ") .trim (.split " ") seq))
+
+(defn parse-trace [trace]
+  (let [trace-split (split-trace trace)
+        trace-time (get-trace-time trace-split)
+        trace-call (get-trace-call trace-split)]
+    [trace-time trace-call]))
+
+
+(defn parse-traces [traces]
+  (map parse-trace traces))
+
+  
 (defn build-profile [traces]
   traces)
 
