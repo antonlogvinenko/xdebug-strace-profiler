d5e7c0cb21fc1afb50bce375f6328d57287a44f8
Anton Logvinenko <logvinenko@farpost.com>
2012-11-03 19:17:06 +1100

    traces reading implementation

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject xdebug-strace-profiler "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :main xdebug-strace-profiler.core)
--- src/xdebug_strace_profiler/core.clj
+++ src/xdebug_strace_profiler/core.clj
@@ -1,1 +1,23 @@
-(ns xdebug-strace-profiler.core)
+(ns xdebug-strace-profiler.core
+  (:use [clojure.java.io :only (file reader)])
+  (:use [clojure.core :only (read-line)])
+  (:import [java.io File]))
+
+
+(defn get-traces [file-path]
+  (-> file-path file reader line-seq))
+
+(defn parse-traces [file]  
+  file)
+
+(defn build-profile [traces]
+  traces)
+
+(defn xml-profile [profile]
+  profile)
+
+(defn -main [file-path]
+  (-> file-path get-traces parse-traces build-profile xml-profile println))
+
+
+
