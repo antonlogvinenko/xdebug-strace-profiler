cc6886b1234e90766ce781ce14a30e6a9f8b80d2
Anton Logvinenko <logvinenko@farpost.com>
2012-11-04 10:45:57 +1100

    Посчитал время, потраченное в каждой функции

--- src/xdebug_strace_profiler/core.clj
+++ src/xdebug_strace_profiler/core.clj
@@ -5,39 +5,58 @@
 
 
 (defn get-traces [file-path]
-  (->> file-path file reader line-seq (drop 1)))
+  (->> file-path file reader line-seq (drop 1) (drop-last 2)))
 
 
-(defn get-trace-position [trace] (nth trace 4))
-(defn get-trace-time [trace] (nth trace 0))
+(defn get-trace-position [trace] (nth trace 4 "xdebug_stop_trace()"))
+(defn get-trace-time [trace] (-> trace (nth 0) Double/parseDouble))
 (defn get-trace-level [trace]
   (let [part-1 (-> trace (.split "->") first)
         part-1-clean (.trim part-1)]
     (- (count part-1) (count part-1-clean))))
-(defn get-trace-call [trace] (nth trace 3))
+(defn get-trace-call [trace] (nth trace 3 "end"))
 
 (defn split-trace [trace]
   (-> trace (.replaceAll " +" " ") .trim (.split " ") seq))
 
 (defn parse-trace [index trace]
   (let [trace-split (split-trace trace)]
-    {:time (get-trace-time trace-split)
+    {:time-start (get-trace-time trace-split)
      :call (get-trace-call trace-split)
      :level (get-trace-level trace)
      :position (get-trace-position trace-split)
      :index index}))
 
-(defn create-profile [traces]
-  (->> traces (map-indexed parse-trace) (group-by :level)))
+(defn parse-traces [traces]
+  (map-indexed parse-trace traces))
+
+
+(defn time-search [profile]
+  (fn [index element]
+    (let [my-level (:level element)
+          start-time (:time-start element)
+          filtered (->> profile
+                        (drop (inc index))
+                        (filter #(-> % :level (<= my-level)))
+                        (take 1))
+          time-new (-> filtered (nth 0 {:time-start start-time}) :time-start)]
+    (assoc element :time-spent (- time-new start-time)))))
 
 
 
-(defn xml-profile [profile] profile)
+(defn calculate-time [profile]
+  (map-indexed (time-search profile) profile))
+
+(defn xml-profile [a] a)
 
 
+(defn dump [x]
+
+  (doseq [e (filter #(-> % :level (<= 33)) x)] (println e))
+(println (count x)))
 
 (defn -main [file-path]
-  (-> file-path get-traces create-profile xml-profile println))
+  (-> file-path get-traces parse-traces calculate-time xml-profile dump))
 
 
 
