58a4820cb51a201335a4dc92cfc4eb4c67d6ef23
Anton Logvinenko <logvinenko@farpost.com>
2012-11-03 20:25:29 +1100

    Распарсил формат xdebug

--- src/xdebug_strace_profiler/core.clj
+++ src/xdebug_strace_profiler/core.clj
@@ -8,36 +8,30 @@
   (->> file-path file reader line-seq (drop 1)))
 
 
-
-
-(defn get-trace-time [trace]
-  (nth trace 0))
-
+(defn get-trace-position [trace] (nth trace 4))
+(defn get-trace-time [trace] (nth trace 0))
 (defn get-trace-level [trace]
-  42)
-
-(defn get-trace-call [trace]
-  (nth trace 3))
+  (let [part-1 (-> trace (.split "->") first)
+        part-1-clean (.trim part-1)]
+    (- (count part-1) (count part-1-clean))))
+(defn get-trace-call [trace] (nth trace 3))
 
 (defn split-trace [trace]
   (-> trace (.replaceAll " +" " ") .trim (.split " ") seq))
 
 (defn parse-trace [trace]
-  (let [trace-split (split-trace trace)
-        trace-time (get-trace-time trace-split)
-        trace-call (get-trace-call trace-split)]
-    [trace-time trace-call]))
+  (let [trace-split (split-trace trace)]
+    [(get-trace-time trace-split)
+     (get-trace-call trace-split)
+     (get-trace-level trace)
+     (get-trace-position trace-split)]))
 
-
-(defn parse-traces [traces]
-  (map parse-trace traces))
+(defn parse-traces [traces] (map parse-trace traces))
 
   
-(defn build-profile [traces]
-  traces)
+(defn build-profile [traces] traces)
 
-(defn xml-profile [profile]
-  profile)
+(defn xml-profile [profile] profile)
 
 (defn -main [file-path]
   (-> file-path get-traces parse-traces build-profile xml-profile println))
